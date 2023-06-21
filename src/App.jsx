ef3d28554214156beb402b34b14b459b1cf5614b
Ngaiti <henfanar@gmail.com>
2023-06-22 00:44:01 +0800

    Navbar routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
 import Home from './pages/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
+function Layout() {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">Home :3</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,9 +29,9 @@ function App() {
     <AuthContext.Provider value={{ token, setToken }}>
       <BrowserRouter>
         <Routes>
-          <Route>
-            <Route element={<Home />} path="/" />
-            <Route element={<Login />} path="/login" />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Route>
         </Routes>
       </BrowserRouter>

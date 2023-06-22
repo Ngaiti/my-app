a3d50a852c4d5c08eb0db3f4e88b29b6dc227c64
Ngaiti <henfanar@gmail.com>
2023-06-22 19:20:49 +0800

    Timer added to welcome page

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -37,6 +37,7 @@ export default function TodoCard({ todo }) {
             );
         }
     };
+
     useEffect(() => {
         return () => {
             clearInterval(timerInterval);
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap"
-import Likecount from "../components/Likecount"
 
 function Welcome() {
+    const [currentTime, setCurrentTime] = useState(new Date());
 
-    const now = new Date()
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     return (
         <div className="welcome">
             <div>
                 <div className='text-center'>
-                    <h1 className="h1-custom"> Hello Sluts! It is now {now.toString()}</h1>
-                    <Likecount />
+                    <h1 className="h1-custom"> Hello Sluts! It is now {currentTime.toString()}</h1>
+
                     <br />
                     <Button size="lg" className="m-5" variant="btn btn-outline-dark" href="/login">Login</Button>
                 </div>

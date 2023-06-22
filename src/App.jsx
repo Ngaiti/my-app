ab0ba0fc2b2073f79e5ce57e9ed133d3d2134d5c
Ngaiti <henfanar@gmail.com>
2023-06-22 13:41:33 +0800

    Navigate to Todo Page and Add Todo page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import Welcome from './pages/Welcome';
+import Home from './pages/Home';
 import Login from './pages/Login'
+import ErrorPage from './pages/Error';
+import AddTodo from './pages/AddTodo';
+
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import { AuthContext } from './AuthContext';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import ErrorPage from './pages/Error';
 import RequireAuth from './components/RequireAuth';
-import Home from './pages/Home';
 import useLocalStorage from 'use-local-storage';
-import Welcome from './pages/Welcome';
+import { TodoContext } from './contexts/TodoContext';
+
 
 function Layout() {
   return (
@@ -27,25 +30,36 @@ function Layout() {
 
 function App() {
   const [token, setToken] = useLocalStorage("token", null);
+  const [todos, setTodos] = useLocalStorage("todos", []);
+
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Welcome />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<ErrorPage />} />
-            <Route
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-              path="/Home" />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <TodoContext.Provider value={{ todos, setTodos }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Welcome />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<ErrorPage />} />
+              <Route
+                element={
+                  <RequireAuth>
+                    <Home />
+                  </RequireAuth>
+                }
+                path="/home" />
+              <Route
+                element={
+                  <RequireAuth>
+                    <AddTodo />
+                  </RequireAuth>
+                }
+                path="/add" />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TodoContext.Provider>
     </AuthContext.Provider>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
-import { Container } from "react-bootstrap";
+import { useContext } from "react";
+import { Card, Col, Badge, Container, Row, Button } from "react-bootstrap";
+import { TodoContext } from "../contexts/TodoContext";
+import AddTodo from "./AddTodo";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const todos = useContext(TodoContext).todos
+    const navigate = useNavigate();
+
+    function GoToAddTodoPage() {
+        navigate("/add")
+    }
     return (
-        <Container>
-            <h1 className="my-3">This is real this is me</h1>
-            <p>Hehe</p>
-        </Container>
+        <>
+            <Container>
+                <h1 className="my-3">Todos</h1>
+                <Row>
+                    <CardGroup todos={todos} />
+                </Row>
+            </Container>
+            <Button onClick={GoToAddTodoPage}> Add Todo</Button>
+        </>
     );
+}
+
+function CardGroup({ todos }) {
+    return todos.map((todo) => {
+        const completed = todo.completed
+        const bg = completed ? "success" : "danger";
+        return (
+            <Col md={4} key={todo.id}>
+                <Card className="my-3">
+                    <Card.Body>
+                        <Card.Title>{todo.title}</Card.Title>
+                        <Card.Text>{todo.description}</Card.Text>
+                        <Badge bg={bg}>{!completed && "Not"} Completed</Badge>
+                    </Card.Body>
+                </Card>
+            </Col>
+        );
+    });
 }
